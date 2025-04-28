35a5c627b983c29e1c5f5abbfdc9392e18fb85d9
tianjingruofeng <2236117954@qq.com>
2025-04-28 20:43:59 +0800

    ✨ feat: add env  inteface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
 /// <reference path="../auto-imports.d.ts" />
 /// <reference path="../components.d.ts" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_API_URL: string
+  readonly BASE_URL: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
